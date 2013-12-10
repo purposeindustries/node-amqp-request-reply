1d4b7391b3386ae18adf581b736ddaf5aecf4669
Bence Dányi <madbence@gmail.com>
2013-12-10 16:17:32 +0100

    Unsubscribe fixed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function wrapper(conn) {
       options.replyTo = anon.name;
       anon.subscribe(function(message, headers, deliveryInfo, job) {
         cb(message, headers, deliveryInfo, job);
-        anon.unsubscibe(deliveryInfo.consumerTag);
+        anon.unsubsrcibe(deliveryInfo.consumerTag);
       });
       if(typeof exchange === 'string') {
         return conn.exchange(exchange, {}, onExchange);
