416c1976fac344f5f6a1d992275c644ca6e5aa23
Bence Dányi <madbence@gmail.com>
2013-12-10 13:34:35 +0100

    Content added

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = function wrapper(conn) {
+  return function(exchange, route, message, options, cb) {
+    function onExchange(exchange) {
+      exchange.publish(route, message, options);
+    }
+    conn.queue('', function(anon) {
+      options.replyTo = anon.name;
+      anon.subscribe(function(message, headers, deliveryInfo, job) {
+        cb(message, headers, deliveryInfo, job);
+        anon.unsubscibe(deliveryInfo.consumerTag);
+      });
+      if(typeof exchange === 'string') {
+        return conn.exchange(exchange, onExchange);
+      }
+      onExchange(exchange);
+    });
+  };
+};
